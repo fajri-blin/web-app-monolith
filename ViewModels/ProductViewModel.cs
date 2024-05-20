28c68bbbd935e7a2bbc90cb794ab22e959bc9102
Fajri <fajriindonesia1945@gmail.com>
2024-05-20 16:07:50 +0700

    adding product view

--- ViewModels/ProductViewModel.cs
+++ ViewModels/ProductViewModel.cs
@@ -4,6 +4,6 @@ namespace web_app.ViewModels;
 
 public class ProductViewModel : TbMProduct
 {
-    public required TbMUnit Unit { get; set; }
-    public required TbTrPrice Price { get; set; }
+    public TbMUnit? Unit { get; set; } = new TbMUnit();
+    public TbTrPrice? Price { get; set; } = new TbTrPrice();
 }
